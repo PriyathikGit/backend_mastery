30f4ea37f0749958c061dca36dcac3715fa3447d
PriyathikGit <techhack237@gmail.com>
2024-11-29 21:40:35 +0530

    data modeling practice

---
+++ day_3_dataModel/Models/todos.model.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const subTodo = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    // main todo have multiple subtodo, so saved in array
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'SubTodo',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const subTodos = mongoose.model('subTodo', subTodo);
