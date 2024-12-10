91c3f80a21174dc3d7d20ec61a6e517fecc9f053
PriyathikGit <techhack237@gmail.com>
2024-12-10 22:03:53 +0530

    utils code( some funtion we gonna use multiple time)

--- main_backend_project/src/index.js
+++ main_backend_project/src/index.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running at :", `${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.log(err, "MongoDB connection failed!!"));
