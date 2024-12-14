7f794419ba2c2d7c442af6322063e660bb3ed74e
PriyathikGit <techhack237@gmail.com>
2024-12-14 21:29:05 +0530

    standard practice of creating routes

--- main_backend_project/src/app.js
+++ main_backend_project/src/app.js
@@ -28,4 +28,11 @@ app.use(express.static("public"));
 
 app.use(cookieParser());
 
+// routes
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter); // end point example : http://localhost:8000/api/v1/users/register
+
 export { app };
